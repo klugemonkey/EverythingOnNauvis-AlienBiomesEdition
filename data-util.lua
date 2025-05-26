5d76b156841f45e15fd2e18ec043aec90dcf89ae
klugemonkey <klugemonkey@spawnchunk.com>
2025-05-26 00:24:37 -0700

    Update data-util.lua
    Added has_expression(type, name) function to check validity of prototypes with expressions

--- data-util.lua
+++ data-util.lua
@@ -17,4 +17,25 @@ function data_util.generate_eon_name(name)
   return "eon_" .. string.gsub(name, "-", "_")
 end
 
+function data_util.has_expression(type, name)
+  if data.raw[type] == nil then
+    log("Missing type definition \"".. type .. "\"")
+    return false
+  end
+  if data.raw[type][name] == nil then
+    log("Missing prototype data.raw[\"".. type .. "\"][\"" .. name .. "\"]")
+    return false
+  end
+  if data.raw[type][name].autoplace == nil then
+    log("Missing autoplace specification for data.raw[\"".. type .. "\"][\"" .. name .. "\"]")
+    return false
+  end
+  if data.raw[type][name].autoplace.probability_expression == nil then
+    log("Missing probability_expression in autoplace specification for data.raw[\"".. type .. "\"][\"" .. name .. "\"]")
+    return false
+  end
+  return true
+end
+
+
 return data_util
